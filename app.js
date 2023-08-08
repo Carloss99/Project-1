338e598d57713c814218d7b7f923503922300ce1
carlos6199 <carlos_fig14@hotmail.com>
2023-08-07 21:02:55 -0400

    updated code to make game restart when completed all words

--- app.js
+++ app.js
@@ -1,103 +1,121 @@
 window.onload = () => {
-fetch("https://random-word-api.vercel.app/api?words=3&length=6&type=capitalized")
-.then((data)=>data.json(), (err) => console.log("no response"))
-.then((w) => {
-    console.log(w)
-    let words = w
-    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    let currentWord = 0
-    let incorrect = 0
-    let matchTotal= 0
-    let wordcontainer = document.querySelector("#word")
-    let enterButton = document.querySelector("#submit")
-    let input = document.querySelector("#input")
-    let hangman = document.querySelector("#hangman")
+        let words 
+        const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        let currentWord = 0
+        let incorrect = 0
+        let matchTotal= 0
+        let wordcontainer = document.querySelector("#word")
+        let enterButton = document.querySelector("#submit")
+        let input = document.querySelector("#input")
+        let hangman = document.querySelector("#hangman")
+        fetchWords()
+        
+        
     
-    setWord()
-    setKeyboard()
-  
-    enterButton.addEventListener('click', (e) =>{
-        let spans = wordcontainer.childNodes
-        let matchFound = false
-        if(input.value.toLowerCase() === words[currentWord].toLowerCase()){
-            spans.forEach((letter) => {
-                letter.classList.remove("display")
-            })
-            matchFound = true
+        enterButton.addEventListener('click', (e) =>{
+            if(input.value === ""){
 
-            setTimeout(() =>{
-                currentWord++
-                setWord()
-                hangman.setAttribute('src', `images/stage0.png`)
-                alert("all done!")
-            }, 5000)
-            
-        }
-        for(let i = 0; i < words[currentWord].length; i++){
-            if(input.value.toLowerCase() === words[currentWord][i].toLowerCase()){
-                spans[i].classList.remove("display")
-                matchTotal++
+            }else{
+            let spans = wordcontainer.childNodes
+            let matchFound = false
+            if(input.value.toLowerCase() === words[currentWord].toLowerCase()){
+                spans.forEach((letter) => {
+                    letter.classList.remove("display")
+                })
                 matchFound = true
+
+                setTimeout(() =>{
+                    currentWord++
+                    setWord()
+                    hangman.setAttribute('src', `images/stage0.png`)
+                    alert("all done!")
+                }, 5000)
+                
             }
-
-            }
-    
-
-        if(matchTotal === words[currentWord].length){
-            setTimeout(() =>{
-                currentWord++
-                setWord()
-            }, 5000)
-            
-        }
-
-        if(!matchFound){
-            hangman.setAttribute('src', `images/stage${++incorrect}.png`)
-            
-                if(incorrect === 7){
-                    alert("You Lose")
+            for(let i = 0; i < words[currentWord].length; i++){
+                if(input.value.toLowerCase() === words[currentWord][i].toLowerCase()){
+                    spans[i].firstChild.classList.remove("no-display")
+                    matchTotal++
+                    matchFound = true
                 }
+
+                }
+        
+
+            if(matchTotal === words[currentWord].length){
+                setTimeout(() =>{
+                    if(currentWord === 3){
+                        fetchWords()
+                        hangman.setAttribute('src', `images/stage0.png`)
+                        matchTotal = 0
+                    }else{
+                        currentWord++
+                        matchTotal = 0
+                        setWord()
+                        }
+                    
+                    
+                }, 5000)
+                
+            }
+
+            if(!matchFound){
+                hangman.setAttribute('src', `images/stage${++incorrect}.png`)
+                if(incorrect === 7){
+                    setTimeout(() =>{
+                        incorrect = 0
+                        alert("You Lose")
+                        fetchWords()
+                    }, 1000)
+                    
+                }
+                    
+            }
+
+            input.value = ""
+        }
+        })
+
+        function setKeyboard() {
+            let keyboard = document.querySelector("#keyboard")
+            for(let i = 0; i < alphabet.length; i++){
+                let letter = document.createElement("span")
+                letter.innerHTML = alphabet[i]
+                letter.addEventListener(('click'), (e) =>{
+                    input.value = letter.innerText
+                    
+                })
+                keyboard.appendChild(letter)
+            }
         }
 
-        input.value = ""
-    })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    function setKeyboard() {
-        let keyboard = document.querySelector("#keyboard")
-        for(let i = 0; i < alphabet.length; i++){
-            let letter = document.createElement("span")
-            letter.innerHTML = alphabet[i]
-            keyboard.appendChild(letter)
+        function setWord () {
+            hangman.setAttribute('src', `images/stage0.png`)
+            wordcontainer.innerHTML = ""
+            for(let i = 0; i < words[currentWord].length; i++){
+                let div = document.createElement("div")
+                let span = document.createElement("span")
+                span.innerHTML = words[currentWord][i]
+                span.setAttribute('class', 'no-display')
+                div.appendChild(span)
+                wordcontainer.appendChild(div)
+            }
         }
-    }
 
-
-    function setWord () {
-        wordcontainer.innerHTML = ""
-        for(let i = 0; i < words[currentWord].length; i++){
-            let span = document.createElement("span")
-            span.innerHTML = words[currentWord][i]
-            span.setAttribute('class', 'display')
-            wordcontainer.appendChild(span)
+        function fetchWords(){
+            fetch("https://random-word-api.vercel.app/api?words=3&length=6&type=capitalized")
+            .then((data)=>data.json(), (err) => console.log("no response"))
+            .then((w) => {
+                console.log(w)
+                words = w
+                setWord()
+                setKeyboard()
+            
+        })
         }
-    }
-})
-
+    
+    
 
 
 
